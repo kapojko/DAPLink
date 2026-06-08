6f58a8d286e7fa18dc47c5237037145495c7ad79
Yuriy Kapoyko <yury.kapoyko@gmail.com>
2026-06-08 22:23:27 +0300

    gcc: Fix build with Arm GNU Toolchain 11.3 and later
    - Add -Wl,--no-warn-rwx-segment to linker flags (GCC 12+)
    - Add weak stubs for newlib functions required by newer toolchains

--- source/daplink/sdk_stub.c
+++ source/daplink/sdk_stub.c
@@ -25,3 +25,21 @@ __WEAK void sdk_init()
 {
     // Do nothing
 }
+
+/*
+ * Work-around for a link-time issue with Arm GNU Toolchain
+ * in version 11.3 and later.
+ */
+#ifdef __GNUC__
+#if ((__GNUC__ > 11) || \
+     ((__GNUC__ == 11) && (__GNUC_MINOR__ >= 3)))
+__WEAK void _close() {}
+__WEAK void _fstat() {}
+__WEAK void _getpid() {}
+__WEAK void _isatty() {}
+__WEAK void _kill() {}
+__WEAK void _lseek() {}
+__WEAK void _read() {}
+__WEAK void _write() {}
+#endif
+#endif
